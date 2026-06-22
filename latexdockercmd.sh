b4541d56b6eb8130df5e1837b18420acabc3e695
Zachary Bowditch <zdbowditch777@gmail.com>
2026-06-22 14:20:33 +0200

    - added install checks

--- latexdockercmd.sh
+++ latexdockercmd.sh
@@ -2,5 +2,8 @@
 
 # See https://hub.docker.com/r/blang/latex
 
+# Install full texlive package
+sudo apt install texlive-full latexmk
+
 IMAGE=blang/latex:ubuntu
 exec docker run --rm -i --user="$(id -u):$(id -g)" --net=none -v "$PWD":/data "$IMAGE" "$@"
--- user-monitoring/user-catcher/run.sh
+++ user-monitoring/user-catcher/run.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
-PYTHON_PATH=$HOME/envs/usermon/bin/python
+set -e
+
+if ! command -v ffmpeg >/dev/null 2>&1; then
+    sudo apt update
+    sudo apt install -y ffmpeg
+fi
+
+if ! command -v eog >/dev/null 2>&1; then
+    sudo apt update
+    sudo apt install -y eog
+fi
+
+PYTHON_PATH=$HOME/micromamba/envs/usermon/bin/python
 
 sudo MOUSE_EVENT=$(./find-mouse.event.sh) $PYTHON_PATH mouse-detect.py
