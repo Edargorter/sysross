3ca4861a1f7df2b9c4a3bdef804c203b26dabe34
Zachary Bowditch <zdbowditch777@gmail.com>
2026-06-22 15:05:44 +0200

    - experimenting

--- user-monitoring/user-catcher/run.sh
+++ user-monitoring/user-catcher/run.sh
@@ -12,6 +12,7 @@ if ! command -v eog >/dev/null 2>&1; then
     sudo apt install -y eog
 fi
 
-PYTHON_PATH=$HOME/micromamba/envs/usermon/bin/python
+# PYTHON_PATH=$HOME/micromamba/envs/usermon/bin/python
 
-sudo MOUSE_EVENT=$(./find-mouse.event.sh) $PYTHON_PATH mouse-detect.py
+# sudo MOUSE_EVENT=$(./find-mouse-event.sh) $PYTHON_PATH mouse-detect.py
+MOUSE_EVENT=$(./find-mouse-event.sh) python mouse-detect.py
